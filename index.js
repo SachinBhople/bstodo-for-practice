1b17f1b71aed19e84e78535d2ff1fc35dd7a0b3e
sachin <sachinb0841@gmail.com>
2024-06-13 11:39:45 +0530

    index path update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config({ path: "./.env" })
 
 const app = express()
